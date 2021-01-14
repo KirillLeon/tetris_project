1fbc9504d801fe9f5948370c3465cffe8c78bcac
Kirill <kirill.leon143@gmail.com>
2021-01-15 00:27:54 +0300

    Third comit Add animation variables, background images and fonts

--- main.py
+++ main.py
@@ -33,3 +33,14 @@ figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
 figures = [[pygame.Rect(x + W // 2, y + 1, 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
 figure_rect = pygame.Rect(0, 0, TILE - 2, TILE - 2)
 field = [[0 for i in range(W)] for j in range(H)]
+
+# Значения для анимации
+anim_count, anim_speed, anim_limit = 0, 60, 2000
+
+# Изображения для фона
+bg = pygame.image.load('img/bg.jpg').convert()
+game_bg = pygame.image.load('img/bg2.jpg').convert()
+
+# Шрифты для игры
+main_font = pygame.font.Font('font/font.ttf', 65)
+font = pygame.font.Font('font/font.ttf', 45)
